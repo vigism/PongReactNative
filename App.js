31b23a2c7378f46d9c06a01377b85b23553626b0
vigism <rushilvig@gmail.com>
2019-12-04 22:02:46 -0800

    Added walls and red square

--- App.js
+++ App.js
@@ -1,22 +1,75 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { Component } from 'react';
+import { StyleSheet, View, StatusBar } from 'react-native';
+import Matter from "matter-js";
+import { GameEngine } from "react-native-game-engine";
+import Bird from './src/Bird';
+import Wall from './src/Wall';
+import Constants from './src/Constants';
+import Physics from './src/Physics';
 
 export default class App extends Component{
 
+  constructor(props){
+    super(props);
+
+    this.state={
+      running: true
+    }
+
+    this.gameEngine = null;
+    this.entities = this.setupWorld();
+  }
+
+  setupWorld = () => {
+    let engine = Matter.Engine.create({
+      enableSleeping:false
+    });
+    let world = engine.world;
+
+    let bird = Matter.Bodies.rectangle(Constants.MAX_WIDTH/4,
+      Constants.MAX_HEIGHT/2, 50, 50);
+
+      let floor = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, Constants.MAX_HEIGHT - 25, Constants.MAX_WIDTH, 50, { isStatic: true });
+      let ceiling = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, 25, Constants.MAX_WIDTH, 50, { isStatic: true });
+
+      Matter.World.add(world, [bird, floor, ceiling]);
+
+
+      return {
+        physics: {engine: engine, world: world},
+        bird: {body:bird, size:[50,50], color:'red',renderer: Bird},
+        floor: { body: floor, size: [Constants.MAX_WIDTH, 50], color: "green", renderer: Wall },
+        ceiling: { body: ceiling, size: [Constants.MAX_WIDTH, 50], color: "green", renderer: Wall },
+      }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-            <Root />
-          </View>
-    );  
-  }
+        <View style={styles.container}>
+            <GameEngine
+                ref={(ref) => { this.gameEngine = ref; }}
+                style={styles.gameContainer}
+                running={this.state.running}
+                entities={this.entities}
+                systems={[Physics]}>
+                <StatusBar hidden={true} />
+            </GameEngine>
+        </View>
+    );
+}
   
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-   
+      flex: 1,
+      backgroundColor: '#fff',
+  },
+  gameContainer: {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
   },
 });
